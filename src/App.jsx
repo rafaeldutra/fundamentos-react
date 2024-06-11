d565763e887c3c922e11b9f66e340d8eefacb815
Rafael <rafaelxdutra@gmail.com>
2024-06-11 13:49:38 -0300

    Adicionado style por parametro no componente Card

--- src/App.jsx
+++ src/App.jsx
@@ -12,24 +12,24 @@ const App =  () => (
         <h1>Fundamentos React </h1>
 
         <div className='Cards'>
-            <Card titulo="#04 - Aletorio">
+            <Card titulo="#04 - Aletorio" color="#FA6900">
                 <Aleatorio
                     maior={100}
                     menor={1}
                 />
             </Card>
-            <Card titulo="#03 - Fragmento">
+            <Card titulo="#03 - Fragmento" color="#E94C6F">
                 <Fragmento />
             </Card>
 
-            <Card titulo="#02 - Com parametro">
+            <Card titulo="#02 - Com parametro"  color="#E8B71A">
                 <ComParametro
                     titulo="Situacao do Aluno"
                     aluno="Pedro"
                     nota={8.7} />
             </Card>
 
-            <Card titulo="#01 - Primeiro componente">
+            <Card titulo="#01 - Primeiro componente"  color="#588C73">
                 <Primeiro></Primeiro>
             </Card>
         </div>
