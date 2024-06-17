b82327ffb127a39efe0c29ffb6ed587ea482d1cb
Rafael <rafaelxdutra@gmail.com>
2024-06-16 21:55:32 -0300

    Contador utilizando class

---
+++ src/components/contador/Contador.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+class Contador extends Component {
+    state = {
+        numero: this.props.numeroInicial
+    };
+
+
+    inc = () => {
+        this.setState({
+            numero: this.state.numero + 1,
+        });
+    }
+    dec = () => {
+        this.setState({
+            numero: this.state.numero - 1,
+        });
+    }
+    
+
+    render() {
+            return (
+                <div>
+                    <h2>Contador</h2>
+                    <h3>{this.state.numero}</h3>
+                    <button onClick={this.inc}>+</button>
+                </div>
+            )
+        }
+
+
+    }
+
+export default Contador
