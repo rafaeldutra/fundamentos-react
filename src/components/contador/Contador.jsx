8b3df225d424b709542aed992c50c4d6ff759dd7
Rafael <rafaelxdutra@gmail.com>
2024-06-25 14:03:03 -0300

    Contador com subcomponentes, Botoes, Display, PassoForm, com comunicacao direta e indireta.

--- src/components/contador/Contador.jsx
+++ src/components/contador/Contador.jsx
@@ -1,29 +1,42 @@
+import "./Contador.css";
 import React, { Component } from 'react'
+
+import Display from "./Display";
+import Botoes from "./Botoes";
+import PassoForm from "./PassoForm";
 
 class Contador extends Component {
     state = {
-        numero: this.props.numeroInicial
+        numero: this.props.numeroInicial || 0,
+        passo: this.props.passoInicial || 5,
     };
 
 
     inc = () => {
         this.setState({
-            numero: this.state.numero + 1,
+            numero: this.state.numero + this.state.passo,
         });
     }
     dec = () => {
         this.setState({
-            numero: this.state.numero - 1,
+            numero: this.state.numero - this.state.passo,
         });
+    }
+
+    setPasso = (novoPasso) => {
+        this.setState({
+            passo: novoPasso,
+        })
     }
     
 
     render() {
             return (
-                <div>
+                <div className="Contador">
                     <h2>Contador</h2>
-                    <h3>{this.state.numero}</h3>
-                    <button onClick={this.inc}>+</button>
+                    <Display numero={this.state.numero}/>
+                    <PassoForm passo={this.state.passo} setPasso={this.setPasso} />
+                    <Botoes setInc={this.inc} setDec={this.dec} />
                 </div>
             )
         }
