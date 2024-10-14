4f775e10b0a0e7258fc0bd2e631553a3d72722ec
Rafael <cast.rafael@sebraego.com.br>
2024-10-14 13:18:53 -0300

    Componente Mega Sena, finalizado

--- src/components/mega/Botao.jsx
+++ src/components/mega/Botao.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const  Botao = props => {
     return (
         <div>
-            <button onClick={props.gerarNumerosMegaSena}>Gerar!</button>
+            <button onClick={props.gerarNumerosMegaSena}>Gerar Números!</button>
         </div>
     )
 }
--- src/components/mega/QuantidadeForm.jsx
+++ src/components/mega/QuantidadeForm.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import './Mega.css'
 
 
 const QuantidadeForm = props =>  {
     return (
         <div>
             <div>
-                        <label htmlFor="quantidadeInput">Quantidade:</label>
+                        <label htmlFor="quantidadeInput" className="Mega">Quantidade:</label>
                         <input 
                             type="number" 
-                            min="1"
-                            max="20"
+                            min="6"
+                            max="15"
                             id="quantidadeInput" 
                             value={props.quantidadeNumerosPassados} 
                             onChange={e => props.setQuantidadeNumeros(+e.target.value)}
